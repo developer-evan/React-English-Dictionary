385c21b4d2e2be68c420602d27730ff0bfdf2272
Evans-254 <71435810+Evans-254@users.noreply.github.com>
2023-05-29 10:53:06 -0700

    added styling using tailwind css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-// import './App.css';
+// import './index.css';
 import React, { useState } from "react";
 import axios from "axios";
 import Words from "./components/Words";
@@ -24,24 +24,28 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+    <div className="bg-slate-950 text-white items-start  p-8">
+      <div className="flex mt-2 pt-5  ml-10">
+      <input  className="bg-sky-500" value={keyword} onChange={(e) => setKeyword(e.target.value)}  />
+     <div > 
       <button
-        className="button"
+        className="text-sky-500 "
         type="submit"
         onClick={handleSearch}
         value="search"
       >
-        search
+         Search
       </button>
       <button
         disabled={!result}
-        className="button"
+        className="bg-red"
         type="submit"
         onClick={handleClear}
       >
         Clear
       </button>
+     </div>
+      </div>
       {result && <Words {...{ result }} />}
     </div>
   );
--- src/components/Words.js
+++ src/components/Words.js
@@ -13,24 +13,24 @@ console.log({e});
   return (
     <div>
       <div>
-        <h3>Word</h3>
-        <div>
-            <p>{word}</p>
-            <p>{meanings[0].partOfSpeech}</p>
-            <p>{phonetics[0].text}</p>
+        <h3 className='text-sky-500 py-2'>Word</h3>
+        <div className='flex'>
+            <p className='px-2'>{word}</p>
+            <p className='px-2'>{meanings[0].partOfSpeech}</p>
+            <p className='px-2'>{phonetics[0].text}</p>
             <div >
-            <p onClick={playAudio}>
+            <p onClick={playAudio} className='bg-sky-500 text-white p-1'>
                 {""}
                 play audio{""}
             </p>
         </div>
       </div>
       <div>
-        <h3>Meaning</h3>
+        <h3 className='text-sky-500 py-2' >Meaning</h3>
         <p>{meanings[0].definitions[0].definition}</p>
       </div>
       <div>
-        <h2>Synonyms</h2>
+        <h2 className='text-sky-500 py-2'>Synonyms</h2>
         <p>
             {meanings[0].synonyms.map((item,index) => {
                 return <p key={index}>{item}</p>
